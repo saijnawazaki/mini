04701160a49199782c1b443e0587ca92adf0d27a
saijnawazaki <33802476+saijnawazaki@users.noreply.github.com>
2017-12-13 09:17:58 +0700

    Add Sei Default Controller
    Like Code Igniter

--- app/config/config.php
+++ app/config/config.php
@@ -41,6 +41,8 @@ if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
  * then replace this line with full URL (and sub-folder) and a trailing slash.
  */
 
+define('sei_default_controller', 'HomeController');
+
 define('URL_PUBLIC_FOLDER', 'public');
 define('URL_PROTOCOL', 'http://');
 define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
